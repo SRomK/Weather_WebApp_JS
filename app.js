6ecd745bcff6e83f6fdb218c18810efbb6bae31b
Sofia <75990610+SRomK@users.noreply.github.com>
2022-11-30 22:42:53 +0100

    implemented map for forecast cards

--- app.js
+++ app.js
@@ -1,17 +1,94 @@
 const result = document.getElementById("result");
 const searchBtn = document.getElementById("search-btn");
 const cityRef = document.getElementById("city-input");
+const cardContainer = document.getElementById("icons-container");
+const fiveDaysTitle = document.getElementById("h1-forecast");
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+const afternoon = "12:00:00";
+const d = new Date();
+
+// console.log({ d })
 //const forecastContainer = document.getElementById("forecast");
 
 const RegExpression = /^[a-zA-Z\s]*$/;
 
 const roundedNum = (data) => Math.round(data);
 
+function checkDay(day) {
+    if (day + d.getDay() > 6) {
+        return day + d.getDay() - 7;
+    }
+    else {
+        return day + d.getDay();
+    }
+};
+
+function dayAppears() {
+    for (let i = 1; i < 6; i++) {
+        document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)];
+    }
+};
+
+const getDataForecast = (apiUrl) => {
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+
+            fiveDaysTitle.innerHTML = `5 days weather forecast`;
+
+            const transformData = data.list.map((item) => {
+                // console.log({ dataAnteriorEntero: item.dt_txt })
+                // console.log({ DatoAnteriorConSplit: item.dt_txt.split(" ") })
+                // console.log({ DatoAnteriorConSplitMasCorchete1: item.dt_txt.split(" ")[1] })
+                return {
+                    ...item,
+                    dt_txt: item.dt_txt.split(" ")[1]
+                }
+            }
+            )
+            // console.log({ transformData })
+            const onlyFiveDays = transformData.filter((item) => item.dt_txt === afternoon)
+
+            // console.log({ onlyFiveDays })
+
+            const printCard = onlyFiveDays.map((item, index) =>
+                `   
+            <div class="icons">
+                <p class="weather" id="day${index + 2}"></p>
+                <div class="icon-img">
+                    <img src="./img/icons/${item.weather[0].icon}.png" class="img-forecast" id="img5" alt="icon weather forecast">
+                </div>
+                <p class="minValues" id="day${index + 2}Min">Min: ${roundedNum(Number(item.main.temp_min - 273.15))}°</p>
+                <p class="maxValues" id="day${index + 2}Max">Max: ${roundedNum(Number(item.main.temp_max - 273.15))}°</p>
+            </div>`
+            )
+            cardContainer.innerHTML = printCard
+            dayAppears()
+
+            // for (let i = 0; i < 5; i++) {
+            //     document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min: " + Math.round(Number(transformData[i].main.temp_min - 273.15)) + "°";
+            // }
+            // for (let i = 0; i < 5; i++) {
+            //     document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max: " + Math.round(Number(transformData[i].main.temp_max - 273.15)) + "°";
+            // }
+            // for (let i = 0; i < 5; i++) {
+            //     document.getElementById("img" + (i + 1)).src = "./img/icons/" + transformData[i].weather[0].icon + ".png";
+            // }
+            
+        }) .catch((err) => {
+            fiveDaysTitle.innerHTML = "";
+            cardContainer.innerHTML = "";
+        });
+
+}
+
 const getWeather = () => {
     let cityValue = cityRef.value;
     //If input field is empty
     if (cityValue.trim().length == 0 || !RegExpression.test(cityValue)) {
         result.innerHTML = `<div class="input-notvalid-wrapper"><h3 class="input-notvalid">Please enter a city name</h3></div>`;
+        cardContainer.innerHTML = "";
+        fiveDaysTitle.innerHTML = "";
     }
 
     //If input field is NOT empty
@@ -47,44 +124,21 @@ const getWeather = () => {
               </div>
           </div>
           
-          `;
+          `;    // DATA.COD ES EL CODIGO DE RESPUESTA DE LA API === 200 ES QUE LA RESPUESTA ES SATISFACTORIA DEL PRIMER FETCH
+                // PARA QUE SE EJECUTE EL SEGUNDO FETCH, COMO NECESITO DATOS DEL PRIMERO PARA LA URL, SOLO EJECUTARÉ EL SEGUNDO FETCH CUANDO
+                // EL COD DEL PRIMERO SEA SATISFACTORIO (200)
+                if (data.cod === 200) {
+                    getDataForecast(`https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${api_forecast}`)
+                }
 
-                let forecast_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${api_forecast}`;
-                fetch(forecast_url)
-                    .then((response) => response.json())
-                    .then((data) => {
 
-                        const transformData = data.list.map((item) => {
-                            // console.log({ dataAnteriorEntero: item.dt_txt })
-                            // console.log({ DatoAnteriorConSplit: item.dt_txt.split(" ") })
-                            // console.log({ DatoAnteriorConSplitMasCorchete1: item.dt_txt.split(" ")[1] })
-                            return { //lo que viene despues del return es un objeto,retornamos un objeto ya transformado
-                                ...item, //el spread operator fusiona varias cosas
-                                dt_txt: item.dt_txt.split(" ")[1]  //dt_txt: es porque necesitas indicarle la propiedad del objeto en la api
-                            }
-                        }
-                        )
-                        // console.log({ transformData })
-                        const afternoon = "12:00:00";
-                        const onlyFiveDays = transformData.filter((item) => item.dt_txt === afternoon)
-
-                        console.log({ onlyFiveDays })
-
-                        for (let i = 0; i < 5; i++) {
-                            document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min: " + Math.round(Number(transformData[i].main.temp_min - 273.15)) + "°";
-                        }
-                        for (let i = 0; i < 5; i++) {
-                            document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max: " + Math.round(Number(transformData[i].main.temp_max - 273.15)) + "°";
-                        }
-                        for (let i = 0; i < 5; i++) {
-                            document.getElementById("img" + (i + 1)).src = "./img/icons/" + transformData[i].weather[0].icon + ".png";
-                        }
-                    })
             })
 
 
             .catch(() => {
                 result.innerHTML = `<h3 class="input-notvalid">City not found</h3>`;
+                cardContainer.innerHTML = "";
+                fiveDaysTitle.innerHTML = "";
             });
     }
 };
@@ -94,29 +148,6 @@ function defaultScreen(){
     getInfo();
 }*/
 
-const d = new Date();
-//console.log(d);
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-function checkDay(day) {
-    if (day + d.getDay() > 6) { //0 + 3 (es 3 solo si es miercoles) es mayor que 6
-        return day + d.getDay() - 7;
-    }
-    else {
-        return day + d.getDay(); //1 + 3 = 4 (hoy siendo miercoles 30) para que muestre jueves 
-    }
-};
-//console.log(d.getDay());
-function dayAppears() {
-    for (let i = 1; i < 6; i++) { //porque en españa se empieza por lunes y aumento 1 y mayor que 6 en vez de 5
-        document.getElementById("day" + (i + 1)).innerHTML = weekday[checkDay(i)];
-    }
-};
 
 searchBtn.addEventListener("click", getWeather);
-// window.addEventListener("load", (getWeather) => console.log("Page fully loaded"));
-
-
-
-//el map lo que hace con el bloque de codigo de html (que map interpreta como un array y cada elemento es una carta de html) en este caso es hacer una copia del array y aplicarle una transformación a los elementos y devolver 
-//un array nuevo con los elementos tranformados
+window.addEventListener("load", (getWeather) => console.log("Page fully loaded"));
